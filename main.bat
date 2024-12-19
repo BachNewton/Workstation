2a9ee2b25bf97eaaa0016e09edd96d9629db189c
BachNewton <kyle1235@gmail.com>
2024-12-19 22:24:30 +0200

    Main chrome and Novelty Games workstations

---
+++ main.bat
@@ -0,0 +1,3 @@
+@echo off
+:: Opens Google Chrome with three tabs
+start chrome "https://mail.google.com/mail/u/0/#inbox" "https://calendar.google.com/calendar/u/0/r" "https://web.whatsapp.com/"
---
+++ novelty.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Open Visual Studio Code to Novelty-Games folder
+start /max "" "C:\Program Files\Microsoft VS Code/Code.exe" "C:\Users\Kyle\hobby\Novelty-Games"
+
+REM Open the first Git Bash terminal and start the localhost server
+start "" "C:\Program Files\Git/git-bash.exe" -c "echo Starting localhost server; cd ~/hobby/Novelty-Games; npm start; exec bash"
+
+REM Open the second Git Bash terminal and check git status
+start "" "C:\Program Files\Git/git-bash.exe" -c "echo Check git status; cd ~/hobby/Novelty-Games; git status; exec bash"
+
+REM End script
+exit
