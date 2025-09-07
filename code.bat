08e64212cd7a5fbdd8a4ff362f77a76577d73765
BachNewton <kyle1235@gmail.com>
2025-09-07 23:49:49 +0300

    Update code.bat

--- code.bat
+++ code.bat
@@ -1,18 +1,16 @@
 @echo off
 
-REM Open Visual Studio Code to Novelty-Games folder
-start /max "" "C:\Program Files\Microsoft VS Code/Code.exe" "C:\Users\Kyle\hobby\Novelty-Games"
-
-REM Opens Google Chrome with ChatGPT and Gemini
+:: Opens Google Chrome with ChatGPT and Gemini
 start chrome "https://chatgpt.com/" "https://gemini.google.com/app"
 
-REM Open the first Git Bash terminal and start the localhost server
+:: Open Visual Studio Code to Novelty-Games folder
+start /max "" /b "C:\Program Files\Microsoft VS Code/Code.exe" "C:\Users\Kyle\hobby\Novelty-Games"
+
+:: Open the first Git Bash terminal and start the localhost server
 start "" "C:\Program Files\Git/git-bash.exe" -c "echo Starting localhost server; cd ~/hobby/Novelty-Games; npm start; exec bash"
 
-REM Open the second Git Bash terminal and check git status
+:: Open the second Git Bash terminal and check git status
 start "" "C:\Program Files\Git/git-bash.exe" -c "echo Check git status; cd ~/hobby/Novelty-Games; git status; exec bash"
 
-REM End script
+:: End script
 exit
-
-
